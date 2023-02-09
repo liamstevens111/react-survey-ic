e1306f653c19e8eb12d7b5b694bc636eee950231
Liam Stevens <8955671+liamstevens111@users.noreply.github.com>
2023-02-09 11:18:22 +0700

    [#1] Add ignore rule for stylint to include tailwind due to errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useRoutes } from 'react-router-dom';
 
 import 'dummy.css';
-import 'assets/stylesheets/application.css';
+import 'assets/stylesheets/application.scss';
 
 import routes from 'routes';
 
