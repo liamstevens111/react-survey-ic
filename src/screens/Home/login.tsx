b5647aca0d99d8254549462b700b8502b644918c
Liam Stevens <8955671+liamstevens111@users.noreply.github.com>
2023-02-17 15:33:11 +0700

    [#21] Allow form to send user/password to API for token. Use Form onSubmit event instead button click

--- src/screens/Home/login.tsx
+++ src/screens/Home/login.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import AuthAdapter from 'adapters/authAdapter';
@@ -6,26 +7,34 @@ import Button from 'components/Button';
 import Input from 'components/Input';
 import { setToken } from 'helpers/userToken';
 
-// TODO: Remove when login functionality implemented in #8
-const tempHandler = () => {
-  return undefined;
-};
-
-// TODO: Remove after testing
-const performLogin = async () => {
-  const response = await AuthAdapter.login({ email: 'placeholder', password: 'placeholder' });
-
-  const {
-    attributes: { access_token: accessToken, refresh_token: refreshToken },
-  } = await response.data;
-
-  setToken({ accessToken: accessToken, refreshToken: refreshToken });
-};
-
 function LoginScreen() {
   const { t } = useTranslation('translation');
 
-  performLogin();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const performLogin = async () => {
+    const response = await AuthAdapter.login({ email: email, password: password });
+
+    const {
+      attributes: { access_token: accessToken, refresh_token: refreshToken },
+    } = await response.data;
+
+    setToken({ accessToken: accessToken, refreshToken: refreshToken });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    performLogin();
+  };
 
   return (
     <>
@@ -33,22 +42,30 @@ function LoginScreen() {
       <p data-test-id="login-header" className="text-white opacity-50 my-8">
         {t('login.sign_in')} to Nimble
       </p>
-      <form>
-        <Input name="email" label={t('login.email')} type="text" className="block h-14 w-80 my-3" onInputChange={tempHandler} />
+      <form onSubmit={handleSubmit}>
+        <Input
+          name="email"
+          label={t('login.email')}
+          type="text"
+          value={email}
+          className="block h-14 w-80 my-3"
+          onInputChange={(e) => handleEmailChange(e)}
+        />
         <div className="relative w-80">
           <Input
             name="password"
             label={t('login.password')}
             type="password"
+            value={password}
             className="block h-14 w-80 my-3"
-            onInputChange={tempHandler}
+            onInputChange={(e) => handlePasswordChange(e)}
           />
           {/* Change to React Router Link when implement #17 */}
           <a href="." className="absolute text-white opacity-50 my-8 left-60 top-5">
             {t('login.forgot_password')}
           </a>
         </div>
-        <Button type="button" text={t('login.sign_in')} className="h-14 w-80" onButtonClick={tempHandler} />
+        <Button text={t('login.sign_in')} className="h-14 w-80" />
       </form>
     </>
   );
