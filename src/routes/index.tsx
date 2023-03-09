afb622b55f02b53eaf2c997c431108b9c81fe2d1
Liam Stevens <8955671+liamstevens111@users.noreply.github.com>
2023-03-09 14:47:47 +0700

    [#17] Add PasswordReset page and form

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,16 @@ import { RouteObject } from 'react-router-dom';
 import PrivateRoutes from 'components/PrivateRoutes';
 import HomeScreen from 'screens/Home';
 import LoginScreen from 'screens/Login';
+import ResetPasswordScreen from 'screens/ResetPassword';
 
 const routes: RouteObject[] = [
   {
     path: '/login',
     element: <LoginScreen />,
+  },
+  {
+    path: '/reset-password',
+    element: <ResetPasswordScreen />,
   },
   {
     element: <PrivateRoutes />,
